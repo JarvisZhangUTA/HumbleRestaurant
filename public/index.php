cc772c9720e056e375bf9016d6535d9c81b6bd85
Jarvis <zhangjw.uta@gmail.com>
2017-08-25 08:14:02 -0500

    composer changes

--- public/index.php
+++ public/index.php
@@ -10,8 +10,7 @@
 | is ready to receive HTTP / Console requests from the environment.
 |
 */
-echo exec('whoami');
-die();
+
 $app = require __DIR__.'/../bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
